fce5af42ef5cdd3b8ac405f48bd4180699fdf65e
vzdrizhni <vzdrizhni@gmail.com>
2020-08-10 20:53:44 +0500

    Filter method implementation

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -3,6 +3,7 @@ import React, {
 } from 'react';
 import './App.css';
 import CardList from './components/card-list/card-list.component'
+import { SearchBox } from '../src/components/search-box/search-box.component'
 
 class App extends Component {
 
@@ -10,7 +11,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -21,9 +23,12 @@ class App extends Component {
   };
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className='App'>
-        <CardList monsters={this.state.monsters} />
+        <SearchBox placeholder='search monsters' handleChange = {e => this.setState({searchField: e.target.value})} />
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
--- monsters-rolodex/src/components/card-list/card/card.component.jsx
+++ monsters-rolodex/src/components/card-list/card/card.component.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import './card.styles.css'
 
 export const Card = (props) => (
-    <div>
+    <div className='card-container'>
+        <img src={`https://robohash.org/${props.monster.id}?set=set2`} alt=""/>
         <h1>{props.monster.name}</h1>
+        <p>{props.monster.email}</p>
     </div>
 )
