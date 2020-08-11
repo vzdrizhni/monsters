aab69e051e2110772aeb93527a6803dde91af86d
vzdrizhni <vzdrizhni@gmail.com>
2020-08-11 14:22:48 +0500

    Updates

---
+++ precache-manifest.67aa23250a04536b86d99e6e9dee1e49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49bb134d70bb4a9066025ec8574e9462",
+    "url": "/vzdrizhni/monsters/index.html"
+  },
+  {
+    "revision": "03bc4891a90ff5307a09",
+    "url": "/vzdrizhni/monsters/static/css/main.d2c6a786.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/vzdrizhni/monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vzdrizhni/monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03bc4891a90ff5307a09",
+    "url": "/vzdrizhni/monsters/static/js/main.329d6c00.chunk.js"
+  },
+  {
+    "revision": "9c3e9a3c7e73e580af92",
+    "url": "/vzdrizhni/monsters/static/js/runtime-main.e562bdba.js"
+  }
+]);
